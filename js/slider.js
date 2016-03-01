ef5b4b3aa0274f4dc64a10019174484013fce4f6
dracon <dracon64@gmail.com>
2016-03-01 10:21:36 +0100

    Slide right

--- js/slider.js
+++ js/slider.js
@@ -2,56 +2,75 @@
  *  * Created by dracon on 28.02.2016.
  *  */
 
+(function ($) {
+  $.fn.dsSlider = function (options) {
 
-var Slider = (function (jQuery) {
-  var elements = $('.slides li')
+    var defaults = {
+      duration: 10000,
+      direction: "right"
+    };
 
-  var config = {
-    duration: 10000
-  }
-
-  function init () {
-    console.log(elements);
-    loopItems(elements);
-  }
+    //Extend default options
+    var options = $.extend(defaults, options);
 
 
-  function slideSwitch() {
-    var $active = $('#slideshow').find('li.active');
+    return this.each(function () {
+      var obj = $(this);
+      loopItems(obj);
+    });
 
-    if ( $active.length == 0 ) $active = $('#slideshow').find('li:last');
 
-    //Er active.next() > 0?
-    var $next =  $active.next().length ? $active.next()
-      : $('#slideshow').find('li:first');
-
-    $active.addClass('last-active')
-      .animate({opacity: 0.0}, 1500);
-
-    $next.css({opacity: 0.0})
-      //.css({"transform": "translateX(-100%)"})
-      .addClass('active slide')
-      //.animate({"transform":"translateX(100%)"},{queue: false, duration: 1000})
-      .animate({opacity: 1.0}, 2000, function() {
-        console.log('Jeg er her!');
-        $active.removeClass('active last-active slide');
-      })
+    function slideLeft(obj) {
+      var $active = $(obj).find('li.active');
+      if ($active.length == 0) $active = $(obj).find('li:last');
+      //Er active.next() > 0?
+      var $next = $active.next().length ? $active.next()
+          : $(obj).find('li:first');
+      $active.addClass('last-active')
+          .animate({opacity: 0.0}, options.duration / 2);
+      $next.css({opacity: 0.0})
+          //.css({"transform": "translateX(-100%)"})
+          .addClass('active slide-left')
+          //.animate({"transform":"translateX(100%)"},{queue: false, duration: 1000})
+          .animate({opacity: 1.0}, 2000, function () {
+            console.log('Jeg er her!');
+            $active.removeClass('active last-active slide-left');
+          })
       ;
-  }
+    }
+
+    function slideRight(obj) {
+      var $active = $(obj).find('li.active');
+      if ($active.length == 0) $active = $(obj).find('li:last');
+      var $next = $active.next().length ? $active.next()
+          : $(obj).find('li:first');
+      $active.addClass('last-active')
+          .animate({opacity: 0.0}, options.duration / 2);
+      $next.css({opacity: 0.0})
+          //.css({"transform": "translateX(-100%)"})
+          .addClass('active slide-right')
+          .animate({opacity: 1.0}, 2000, function () {
+            console.log('Jeg er her!');
+            $active.removeClass('active last-active slide-right');
+          })
+      ;
+    }
 
 
-  function loopItems(){
-    setInterval(function () {
-      slideSwitch();
-    }, config.duration); //TODO: dynamisk duration
+    function loopItems(obj) {
+      setInterval(function () {
+        if (options.direction === "left") {
+          $(obj).children().css({left: "-100%"});
+          slideLeft(obj);
+        }
+        if (options.direction === "right"){
+          $(obj).children().css({left: "+100%"});
+          slideRight(obj);
+        }
+      }, options.duration); //TODO: dynamisk duration
+    }
+  };
+})(jQuery);
 
 
-  }
-
-  return {
-    init: init
-  }
-}(Slider || {}, jQuery));
-
-Slider.init();
 
